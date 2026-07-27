2410c46170723942da4a94688ac18365c87e0d1e
prateekchaurasiya2007-kld <prateekchaurasiya2007@gmail.com>
2026-07-27 16:15:47 +0530

    day3

--- day3.js
+++ day3.js
@@ -1,9 +1,20 @@
 //Asynchronous js using async/await
-async function test(){
-   console.log("2:message");
-   console.log("3:message");
-   console.log("4:message");
-}
-  console.log("1:message");
-  test()
-   console.log("5:message");
+// async function test(){
+//    console.log("2:message");
+//    console.log("3:message");
+//    console.log("4:message");
+// }
+//   console.log("1:message");
+//   test()
+//    console.log("5:message");
+   //create promises that resolve with Assisgnment submitted
+   //and print using then(),reject with submission failed
+ //async function that display user data
+  async function test(){
+  await console.log("ID");
+     await console.log("MANGER");
+     console.log("27YEAR");
+ }
+ console.log("FROM ABES");
+ test()
+ console.log("5");
