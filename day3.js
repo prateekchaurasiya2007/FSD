0d8d5d6c6076fa4dcf7c975c57956c9c5710e534
prateekchaurasiya2007-kld <prateekchaurasiya2007@gmail.com>
2026-07-27 16:27:48 +0530

    day3

--- day3.js
+++ day3.js
@@ -10,11 +10,27 @@
    //create promises that resolve with Assisgnment submitted
    //and print using then(),reject with submission failed
  //async function that display user data
-  async function test(){
-  await console.log("ID");
-     await console.log("MANGER");
-     console.log("27YEAR");
+//   async function test(){
+//   await console.log("ID");
+//      await console.log("MANGER");
+//      console.log("27YEAR");
+//  }
+//  console.log("FROM ABES");
+//  test()
+//  console.log("5");
+ async function data() {
+    await console.log("2:message");
+   const response=fetch("./studentdata.json");
+   console.log(response.status);
+   const std=(await response).json();
+   return std;
+   await console.log("3:message");
+   console.log("4:message");    
  }
- console.log("FROM ABES");
- test()
- console.log("5");
+ console.log("1:message");
+ data()
+.then(()=>{
+console.log(res);
+}).catch(()=>{
+    console.log()
+})
