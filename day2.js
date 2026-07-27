4a70d7103a6387bbcc5dcc8bd44148f7e66f0060
prateekchaurasiya2007-kld <prateekchaurasiya2007@gmail.com>
2026-07-27 10:20:37 +0530

    day3.js

--- day2.js
+++ day2.js
@@ -1,20 +1,39 @@
 //callback:function
-function hello(n1,n2,callback){
-console.log("Hello world");
-callback();    
-}
+// function hello(n1,n2,callback){
+// console.log("Hello world");
+// callback();    
+// }
 
-let a=10;
-let b=20;
-console.log(hello(a,b,sayHi));
-console.log(hello(a,b,sayHello()))
-console.log(hello(a,b0,function demo(){
-  console.log("callback is calling")}));
-function sayHi(){
-    console.log("callback function");
-}
-sayHi();
-function sayHello(){
-    console.log("this is 2nd callback function")
-}
-sayHi();
+// let a=10;
+// let b=20;
+// console.log(hello(a,b,sayHi));
+// console.log(hello(a,b,sayHello()))
+// console.log(hello(a,b0,function demo(){
+//   console.log("callback is calling")}));
+// function sayHi(){
+//     console.log("callback function");
+// }
+// sayHi();
+// function sayHello(){
+//     console.log("this is 2nd callback function")
+// }
+// sayHi();
+//Promises
+// const PromiseOne=new Promise((resolve,reject)=>{
+// console.log("successfull data passed.....")
+// });
+// PromiseOne.then((result)=>{
+//     console.log(result);
+
+// }).catch(()=>{
+
+// }
+new Promise(function(resolve,reject){
+setTimeout(function(){
+    let msg=true;
+    if(!msg)
+    console.log("asynchronous task...");
+    resolve();
+})    
+
+}).then().catch()
---
+++ day3.js
@@ -0,0 +1,9 @@
+//Asynchronous js using async/await
+async function test(){
+   console.log("2:message");
+   console.log("3:message");
+   console.log("4:message");
+}
+  console.log("1:message");
+  test()
+   console.log("5:message");
