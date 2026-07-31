b203182d105e72708c456897a416f6861a87f4ba
prateekchaurasiya2007-kld <prateekchaurasiya2007@gmail.com>
2026-07-31 12:25:42 +0530

    day5

--- day5.js
+++ day5.js
@@ -1,12 +1,22 @@
-const EventEmitter=require("events");
-const event=new EventEmitter();
+//const EventEmitter=require("events");
+//const event=new EventEmitter();
 // event.on("greet",()=>{
 //     console.log("this is event emitter");
 // })
-event.once("greet",()=>{
-    console.log("event trigger only one time");
+//event.once("greet",()=>{
+  //  console.log("event trigger only one time");
+//})
+//event.emit("greet");
+//event.emit("greet");
+//event.emit("greet");
+//event.emit("greet");
+class MyEmitter extends EventEmitter{}
+const event=new MyEmitter()
+event.on("greet",(name)=>{
+    console.log('hello ${name}');
 })
-event.emit("greet");
-event.emit("greet");
-event.emit("greet");
-event.emit("greet"); 
+event.on("exit",()=>{
+    console.log("exits mycutom event emitter..");
+})
+event.emit("greet","cse24");
+event.emit(exit);
