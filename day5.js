f044dbc4dbccc48796304c841af00ccc6528ec46
prateekchaurasiya2007-kld <prateekchaurasiya2007@gmail.com>
2026-07-31 12:09:27 +0530

    day5

--- day5.js
+++ day5.js
@@ -1,14 +1,12 @@
-//Event 
-//EventEmitter is class in which we have to use emit("event emit program") method for trigger./create/fire/ event and on ("emit param",callback) 
-//Program 1 and 2
-//emit() on()
-const EventEmitter=require("event");
+const EventEmitter=require("events");
 const event=new EventEmitter();
-event.emit("greet",()=>{
-    console.log("this is event emitter");
-});
+// event.on("greet",()=>{
+//     console.log("this is event emitter");
+// })
+event.once("greet",()=>{
+    console.log("event trigger only one time");
+})
 event.emit("greet");
 event.emit("greet");
 event.emit("greet");
-event.emit("greet");
-event.emit("greet");
+event.emit("greet"); 
