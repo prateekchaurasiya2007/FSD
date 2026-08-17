f577233e5657826bb056e5c595d451c6b9e4f167
prateekchaurasiya2007-kld <prateekchaurasiya2007@gmail.com>
2026-08-17 10:33:01 +0530

    day8

--- HTTPMODULE/day8.js
+++ HTTPMODULE/day8.js
@@ -1,7 +1,7 @@
 //create your own server using http module
 import http from "http";
 const server=http.createServer((req,res)=>{
-    res.writeHead
+    res.writeHead(200,{"content-type":"text/html"})
     res.write("hello world")
 })
 server.listen(3000,()=>{
