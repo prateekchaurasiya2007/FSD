d0eca0b880c5c900c69070962f04fad60d679331
prateekchaurasiya2007-kld <prateekchaurasiya2007@gmail.com>
2026-08-17 10:18:38 +0530

    day7

--- day7.js
+++ day7.js
@@ -11,4 +11,63 @@ async function createFile(){
     {
       console.log("error.......")
     }
-}  
+}then()
+import fs from "fs/promises";
+
+const fileName = "student.txt";
+
+
+// 2. READ
+async function readFile() {
+    try {
+        const data = await fs.readFile(fileName, "utf8");
+
+        console.log("\nFile Content:");
+        console.log(data);
+    } catch (error) {
+        console.log("Error:", error.message);
+    }
+}
+
+
+// 3. UPDATE
+async function updateFile() {
+    try {
+        await fs.appendFile(
+            fileName,
+            "\nCollege: ABES Engineering College............",
+            "utf8"
+        );
+
+        console.log("\nFile updated successfully");
+    } catch (error) {
+        console.log("Error:", error.message);
+    }
+}
+
+
+// 4. DELETE
+async function deleteFile() {
+    try {
+        await fs.unlink(fileName);
+
+        console.log("\nFile deleted successfully");
+    } catch (error) {
+        console.log("Error:", error.message);
+    }
+}
+
+
+// Execute CRUD operations
+async function main() {
+
+    await createFile();
+
+    await readFile();
+
+    await updateFile();
+
+    //await deleteFile();
+}
+
+main();
