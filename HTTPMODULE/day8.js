2211a7d47218d4ac2e9fea56b6e45f0151058c99
prateekchaurasiya2007-kld <prateekchaurasiya2007@gmail.com>
2026-08-18 15:50:05 +0530

    day8

--- HTTPMODULE/day8.js
+++ HTTPMODULE/day8.js
@@ -1,8 +1,9 @@
 //create your own server using http module
 import http from "http";
 const server=http.createServer((req,res)=>{
-    res.writeHead(200,{"content-type":"text/html"})
-    res.write("hello world")
+    res.writeHead(200,{"content-type":"text/html"});
+    res.write("<h1>this is my own server")
+    res.end();
 })
 server.listen(3000,()=>{
     console.log("server is running on port 3000")
