17c458ee7602048d8f286a708b82afa93a4855dd
prateekchaurasiya2007-kld <prateekchaurasiya2007@gmail.com>
2026-08-31 09:46:46 +0530

    day9

--- HTTPMODULE/day9.js
+++ HTTPMODULE/day9.js
@@ -3,5 +3,5 @@ const server = http.createServer((req,res) => {
 
 })
 server.listen(3000,()=> {
-    console.log("server is running on http://localhost")
+    console.log("server iss running on http://localhost")
 })
